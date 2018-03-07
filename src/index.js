8a1dcc8582306e535aed35522004cc7cdaebf195
ngoctam <nguyenngoctamvn@gmail.com>
2018-03-07 08:01:05 +0700

    Start React and Render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// *** Create a new component. This component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const App = function() {
+//   return <div>Hi!</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// *** Arrow function
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// *** http://babeljs.io: with react will be
+// var App = function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hi!"
+//   );
+// };
+
+// *** Take this component's generated HTML and put it on the page (in the DOM)
+
+// React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
